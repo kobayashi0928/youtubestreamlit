331e089a485a3c4d65cd21bf12af2cca68b6bd47
kobayashi <chochichochi0928@gmail.com>
2022-05-26 10:20:35 +0900

    1st commit

--- main5.py
+++ main5.py
@@ -1,10 +1,4 @@
-from cmath import exp
-from platform import python_branch
-from turtle import left, st
 import streamlit as st
-import numpy as np
-import pandas as pd
-from PIL import Image
 import time
 
 st.title('streamlit 入門')
@@ -39,10 +33,5 @@ expander3.write('問い合わせ3回答')
 expander4 = st.beta_expander('問い合わせ4')
 expander4.write('問い合わせ4回答')
 
-# text = st.text_input('あなたの趣味を教えてください。')
-# condetion = st.slider('あなたの今の調子は？', 0, 100, 50)
-
-# 'あなたの趣味：', text
-# 'コンディション：', condetion
 
 
